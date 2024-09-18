451480e8019506ecce7df02e0eaa04caaf1f3c54
hussainsarwari <sarwarihussain7@gmail.com>
2024-09-18 22:40:42 +0430

    finish html to pdf front end

--- resources/views/html_to_pdf.blade.php
+++ resources/views/html_to_pdf.blade.php
@@ -171,42 +171,42 @@
     <div class="relative w-full h-screen mb-20 " >
         <div class="w-[55em] h-[55em] lg:w-[80em] lg:h-[80em] bg-blue-500 shadow-md absolute top-[-18em] right-[-5em] rounded-full"></div>
         <div class="w-full flex-row  flex justify-center absolute  top-[6em] text-white">
-
+            
             <h1 class="w-full  flex justify-center absolute  text-sm lg:text-3xl top-16 lg:top-[6em] text-white">Online HTML to pdf convertor</h1>
             <form action="" method="post" class="flex flex-row mx-5 ">
-                    @csrf
+                @csrf
                 <input class="p-3 shadow-md  w-[20em] lg:w-[50em] h-[3em] rounded-[30em] text-white bg-blue-400  outline-none focus:outline-none focus:bg-blue-400 placeholder:text-white relative top-[7em] lg:top-[21em] right-[-1em]" placeholder="Enter website URL" type="html_to_pdf" name="html_to_pdf" required id="html_to_pdf">
                 <button type="submit" class="relative top-[7em] lg:top-[21em] right-[2em] lg:right-[4em] ">
                     <svg class="mx-2 bi bi-search " fill="#fff" height="20" viewbox="0 0 16 16" width="20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
                     </button>
-            </form>
-        </div>
-        <div class="result w-[20em]  lg:w-[50em] bg-slate-100 shadow-lg rounded-xl flex justify-center flex-col p-3 relative m-auto top-[25em] lg:top-[35em] ">
+                </form>
+            </div>
             
+            <div class="result w-[20em]  lg:w-[50em]   flex justify-center flex-col p-3 relative m-auto top-[25em] lg:top-[35em] ">
           
-            <p class="flex justify-between w-full">
-                <span class="w-full text-sm font-bold text-center lg:text-3xl">Number</span>
-                <span class="w-full text-sm font-bold text-center lg:text-3xl">Size</span>
-                <span class="w-full text-sm font-bold text-center lg:text-3xl">Title</span>
-                <span class="w-full text-sm font-bold text-center lg:text-3xl">Download</span>
-            </p>
-            <hr>
-            <ul>
-                <li class="flex justify-between w-full h-10">
-                    <span class="w-full text-sm font-bold text-center lg:text-xl">1</span>
-                    <span class="w-full text-sm font-bold text-center lg:text-xl">720px</span>
-                    <span class="w-full text-sm font-bold text-center lg:text-xl">Title</span>
-                    <span class="w-full text-sm font-bold text-center lg:text-xl">
-                       <a href="{{url('instagram-download-btn')}}">
-                        <button>Download</button></span>
-                    </a>
-                </li>
-                <hr>
-             
-            </ul>
-            
-        
+           <div class="w-full btn-conteiner">
+                <a class="btn-content" href="#">
+                    <span class="text-sm btn-title">Convert</span>
+                    <span class="icon-arrow">
+                        <svg width="60px" height="40px" viewBox="0 0 66 43" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="arrow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <path id="arrow-icon-one"
+                                    d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                                    fill="#FFFFFF"></path>
+                                <path id="arrow-icon-two"
+                                    d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                                    fill="#FFFFFF"></path>
+                                <path id="arrow-icon-three"
+                                    d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                                    fill="#FFFFFF"></path>
+                            </g>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
         
         </div>
     </div>
