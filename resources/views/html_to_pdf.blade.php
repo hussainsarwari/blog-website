a22fa7115d4272ded6a5885a963c12dc13a16d60
hussainsarwari <sarwarihussain7@gmail.com>
2024-09-18 22:33:31 +0430

    html to pdf front end 2

--- resources/views/html_to_pdf.blade.php
+++ resources/views/html_to_pdf.blade.php
@@ -172,10 +172,10 @@
         <div class="w-[55em] h-[55em] lg:w-[80em] lg:h-[80em] bg-blue-500 shadow-md absolute top-[-18em] right-[-5em] rounded-full"></div>
         <div class="w-full flex-row  flex justify-center absolute  top-[6em] text-white">
 
-            <h1 class="w-full  flex justify-center absolute  text-sm lg:text-3xl top-16 lg:top-[6em] text-white">Instagram online Video Downloader</h1>
-            <form action="" method="post" class="flex flex-row mx-5 search">
+            <h1 class="w-full  flex justify-center absolute  text-sm lg:text-3xl top-16 lg:top-[6em] text-white">Online HTML to pdf convertor</h1>
+            <form action="" method="post" class="flex flex-row mx-5 ">
                     @csrf
-                <input class="p-3 shadow-md  w-[20em] lg:w-[50em] h-[3em] rounded-[30em] text-white bg-blue-400  outline-none focus:outline-none focus:bg-blue-400 placeholder:text-white relative top-[7em] lg:top-[21em] right-[-1em]" placeholder="Enter Video URL" type="instagram-downloader" name="instagram-downloader" required id="instagram-downloader">
+                <input class="p-3 shadow-md  w-[20em] lg:w-[50em] h-[3em] rounded-[30em] text-white bg-blue-400  outline-none focus:outline-none focus:bg-blue-400 placeholder:text-white relative top-[7em] lg:top-[21em] right-[-1em]" placeholder="Enter website URL" type="html_to_pdf" name="html_to_pdf" required id="html_to_pdf">
                 <button type="submit" class="relative top-[7em] lg:top-[21em] right-[2em] lg:right-[4em] ">
                     <svg class="mx-2 bi bi-search " fill="#fff" height="20" viewbox="0 0 16 16" width="20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
