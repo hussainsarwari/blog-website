094610b78728d09483211bb223d4246a0cf398b9
hussainsarwari <sarwarihussain7@gmail.com>
2024-09-18 12:48:36 +0430

    pdf merging

--- resources/views/online_pdf_editor.blade.php
+++ resources/views/online_pdf_editor.blade.php
@@ -186,7 +186,7 @@
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                           </svg>
-                          Choose File
+                          Select PDF File
                         </span>
                         <input type="file" id="pdf" class="absolute inset-0 opacity-0 cursor-pointer">
                       </label>
--- resources/views/online_pdf_merging.blade.php
+++ resources/views/online_pdf_merging.blade.php
@@ -245,86 +245,120 @@
 
 
     {{-- main section --}}
-    <div class="relative w-full h-screen mb-20 ">
-        <div
-            class="w-[55em] h-[55em] lg:w-[80em] lg:h-[80em] bg-blue-500 shadow-md absolute top-[-18em] right-[-5em] rounded-full">
-        </div>
+    <div class="relative w-full h-screen mb-20 " >
+        <div class="w-[55em] h-[55em] lg:w-[80em] lg:h-[80em] bg-blue-500 shadow-md absolute top-[-18em] right-[-5em] rounded-full"></div>
         <div class="w-full flex-row  flex justify-center absolute  top-[6em] text-white">
 
-            <h1 class="w-full  flex justify-center absolute  text-sm lg:text-3xl top-16 lg:top-[6em] text-white">Online
-                PDF Compressor for free</h1>
+            <h1 class="w-full  flex justify-center absolute  text-sm lg:text-3xl top-16 lg:top-[6em] text-white">Online PDF editor for free</h1>
             <form action="" method="post" class="flex flex-col justify-center mx-5 ">
-                @csrf
+                    @csrf
 
-
-                <label
-                    class="relative top-[10em] lg:top-[20em] inline-block cursor-pointer bg-white text-primary font-medium py-2 px-4 border border-blue-500 rounded-lg shadow-sm  transition duration-300 ease-in-out">
-                    <span class="flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" class="w-5 h-5 mr-2">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 4v16m8-8H4" />
-                        </svg>
-                        Upload File
-                    </span>
-                    <input type="file" id="compressor" class="absolute inset-0 opacity-0 cursor-pointer">
-                </label>
-                <span id="errorMessage"
-                    class="text-red-300 absolute top-[14em] md:top-[13em] lg:top-[23.5em] hidden">Please choose pdf
-                </span>
-                <span id="successMessage"
-                    class="text-green-300 absolute top-[14em] md:top-[13em] lg:top-[23.5em] hidden">File added
-                    succesfully </span>
-
+                    
+                    <label class="relative top-[10em] lg:top-[20em] inline-block cursor-pointer bg-white text-primary font-medium py-2 px-4 border border-blue-500 rounded-lg shadow-sm  transition duration-300 ease-in-out">
+                        <span class="flex items-center justify-center">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                          </svg>
+                          Select PDF File
+                        </span>
+                        <input type="file" id="pdf" class="absolute inset-0 opacity-0 cursor-pointer">
+                      </label>
+                      <span id="errorMessage" class="text-red-300 absolute top-[14em] md:top-[12em] lg:top-[23.5em] hidden">Please choose pdf </span>
+                      <span id="successMessage" class="text-green-300 absolute top-[14em] md:top-[12em] lg:top-[23.5em] hidden">File added succesfully </span>
+                
             </form>
         </div>
+        
+<div class=" grid-cols-12 grid-rows-12  w-full h-full  fixed top-0 z-[200] editor_box hidden">
+    <div class="col-span-12 row-span-1 bg-[#333]  py-3 font-bold text-sm md:text-lg  text-white flex items-center justify-between ">
+        
+       <span class="ml-2 text-sm md:text-lg w-[10em]">
+           Online code Editor 
+        </span>
+        <form action="" method="post" class="flex-row hidden mx-5 search_word md:flex">
+            @csrf
+            <input class="p-1 mx-2 text-white border-b text-sm border-blue-500 outline-none bg-[#333] focus:outline-none focus:bg-[#3e3e3e] placeholder:text-white" placeholder="Search word or page" type="search" name="search" required id="search">
+            <button type="submit">
+                <svg class="mx-2 bi bi-search" fill="#fff" height="20" viewbox="0 0 16 16" width="20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
+                </button>
+        </form>
+        <a class='inline menu-trigger2 md:hidden'>
+            <span>Menu</span>
+        </a>
+        
+        </div>
+ 
+    <div class="grid grid-cols-1 col-span-12 row-span-12 bg-slate-100 grid-rows-12">
+        <span class="editor_section font-bold  row-span-1 z-[200] bg-white shadow-lg  justify-evenly items-center  w-full hidden md:flex ">
+          
+            <form action="" method="post" class="flex flex-row mx-5 search_word md:hidden">
+                @csrf
+                <input class="p-1 mx-2 text-[#333] border-b text-[.8em] border-blue-500 outline-none bg-white focus:outline-none focus:border-blue-600 placeholder:text-[#333]" placeholder="Search word or page" type="search" name="search" required id="search">
+                <button type="submit">
+                    <svg class="mx-2 bi bi-search" fill="#333" height="20" viewbox="0 0 16 16" width="20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
+                    </button>
+            </form>
+          
+               
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add New Text">Text</button>
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add New Image">Image</button>
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add Highlight">Highlight</button>
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add Underline">Underline</button>
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add Comment">Comments</button>
+               <button class =" focus:outline-none hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add Shape">Shapes</button>
+               <div class="">
+                <span class="z-[400]  text-[.8em] p-1 hover:text-blue-600 text-blue-400 cursor-pointer scroll-to-section peer " id="watermark"  style="  border: none; cursor: pointer;"    >Watermark</span>
+                <!-- Dropdown menu -->
+                <div class="fixed flex-col hidden p-4 mt-0 bg-white shadow-lg blog rounded-xl peer-hover:flex hover:flex h-fit z-[500]">
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add new WaterMark</button> 
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Remove Watermark </button>
+               </div>
+            </div>
+               <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 " title="Add link">Links</button>
+               <div class="">
+                <span class="z-[400]  text-[.8em] p-1 hover:text-blue-600 text-blue-400 cursor-pointer scroll-to-section peer " id="form_feild"  style="  border: none; cursor: pointer;"    >Form feilds</span>
+                <!-- Dropdown menu -->
+                <div class="fixed flex-col hidden p-4 mt-0 bg-white shadow-lg blog rounded-xl peer-hover:flex hover:flex h-fit z-[500]">
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add Checkbox</button> 
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add Radio button </button>
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add List </button>
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add Date  </button>
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add Number </button>
+                    <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400">Add Textarea </button>
+               </div>
+            </div>
+               <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400 ">Delete</button>
+               <button class=" focus:outline-none zoom-in-btn rounded-[1em]  border-2 border-blue-400 w-[2em] h-[2em] text-blue-400 ">+</button>
+               <button class=" focus:outline-none zoom-out-btn rounded-[1em]  border-2 border-blue-400 w-[2em] h-[2em] text-blue-400">-</button>
+               <button class=" focus:outline-none  hover:text-blue-600 w-fit text-[.8em] p-1 h-[2em] text-blue-400  editor-tools-close-btn ">Close</button>
+         
+        </span>
+            
+            <div class="relative col-span-1 m-auto overflow-hidden show_pdf row-span-10 ">
+                <canvas id="pdf-canvas">
 
+                </canvas>
+                <canvas id="selection-canvas" style="width: 100%;height: 100%;position: absolute;top: 0em;">
+                    
+                </canvas>
+
+            </div>
+            
+            <div class="buttons  col-span-1 row-span-12 p-3 flex items-center justify-evenly bg-white shadow-lg z-[200]" >
+                <button class="focus:outline-none text-[12px] md:text-md border-2  text-blue-400 md:mr-2 border-blue-400 py-[.8em] h-[3em] md:py-1 px-[.8em]  md:px-[6em] rounded-md close-editor-btn">Cancel</button>
+                <button class="focus:outline-none text-[12px] md:text-md previos_page bg-blue-400 text-white py-[.8em] h-[3em] md:py-1 px-[.8em]  md:px-[6em] rounded-md  ">previos page</button>
+                <button class="focus:outline-none text-[12px] md:text-md next_page bg-blue-400 text-white py-[.8em] h-[3em] md:py-1 px-[.8em]  md:px-[6em] rounded-md  ">Next page</button>
+                <button class="focus:outline-none text-[12px] md:text-md bg-blue-400 text-white py-[.8em] h-[3em] md:py-1 px-[.8em]  md:px-[6em] rounded-md  ">Save</button>
+            </div>
+    </div>
+    
+  
+</div>
+    
     </div>
 
-
-
-    {{-- pdf compressor section  --}}
-    <section class="absolute w-full p-2 pdf-compressor top-[25em] lg:top-[35em] hidden">
-        <div class="m-auto w-[20em] lg:w-[60em] bg-slate-50 shadow-xl rounded-md h-[30em] flex flex-col justify-center">
-            <p class="w-full p-2 text-sm font-bold text-center pdf-compression-header md:text-md">Free pdf compression
-                <hr>
-            </p>
-            <div class="container p-2 h-[22em]  flex items-center ">
-                <div class="flex items-center w-full border border-primary  rounded-md p-[1em] justify-evenly">
-                    <span>math</span>
-                    <span>24kb</span>
-                    <span><input type="checkbox" name="select" id="select"></span>
-                </div>
-            </div>
-
-
-
-
-            {{-- btn compression --}}
-            <div class="w-full btn-conteiner">
-                <a class="btn-content" href="#">
-                    <span class="text-sm btn-title">Compress</span>
-                    <span class="icon-arrow">
-                        <svg width="60px" height="40px" viewBox="0 0 66 43" version="1.1"
-                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <g id="arrow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <path id="arrow-icon-one"
-                                    d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
-                                    fill="#FFFFFF"></path>
-                                <path id="arrow-icon-two"
-                                    d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
-                                    fill="#FFFFFF"></path>
-                                <path id="arrow-icon-three"
-                                    d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
-                                    fill="#FFFFFF"></path>
-                            </g>
-                        </svg>
-                    </span>
-                </a>
-            </div>
-        </div>
-
-    </section>
 
 
 
