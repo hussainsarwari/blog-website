900ac97bf146dcdb278db56cd27b342c0f80ea80
hussainsarwari <sarwarihussain7@gmail.com>
2024-10-04 12:47:40 +0430

    img to pdf

---
+++ resources/js/img_to_pdf.js
@@ -0,0 +1,9 @@
+// open box
+
+
+// close box
+const convertbox=document.querySelector(".convert_box");
+const closeBtn=document.querySelector(".close_box_btn");
+closeBtn.addEventListener("click",()=>{
+    convertbox.classList.add("hidden")
+})
