32b070668da62ff0f0a2591391f12bb040c0eee1
hussainsarwari <sarwarihussain7@gmail.com>
2024-10-05 13:02:51 +0430

    video compression 4

--- resources/views/pdf_to_image.blade.php
+++ resources/views/pdf_to_image.blade.php
@@ -185,8 +185,6 @@
                                     <a class="px-1 w-[15em] text-center py-2 text-black hover:bg-gray-300 hover:text-white"
                                         href="{{ url('online-images-compression') }}">images Compression </a>
                                     <a class="px-1 w-[15em] text-center py-2 text-black hover:bg-gray-300 hover:text-white"
-                                        href="{{ url('online-video-compression') }}">video Compression </a>
-                                    <a class="px-1 w-[15em] text-center py-2 text-black hover:bg-gray-300 hover:text-white"
                                         href="{{ url('extract-text-from-image') }}">Extract Text From Image</a>
 
 
