4dd27792492e234cf501e6f6bacacc249b16e6b7
hussainsarwari <sarwarihussain7@gmail.com>
2024-10-16 22:16:26 +0430

    kljlk

--- resources/views/blog/Travel/show_travel_blog_post.blade.php
+++ resources/views/blog/Travel/show_travel_blog_post.blade.php
@@ -436,7 +436,6 @@
 
 
 
-
                     {{-- tags --}}
                     <section aria-label="Tags related to this blog post"
                         class="tags bg-slate-100 p-3 my-5 rounded-md shadow-md text-slate-500">
